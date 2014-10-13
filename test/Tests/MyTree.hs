062e93a04e449f30024df086a66c8eb040e82757
Guy John <guy@rumblesan.com>
2014-10-13 13:57:04 +0000

    Adding tests to prove MyTree functor laws work

--- test/Tests/MyTree.hs
+++ test/Tests/MyTree.hs
@@ -12,7 +12,9 @@ myTreeTests :: Test
 myTreeTests =
   testGroup "MyTree Tests" [
     testCase "Adding data works as expected" test_addingData,
-    testProperty "Functor mapping works" prop_functorFmapWorks
+    testProperty "Functor mapping works" prop_functorFmapWorks,
+    testProperty "Functor fmap with id function is correct" prop_functorWithId,
+    testProperty "Functor fmap is commutative" prop_functorCommutative
   ]
 
 
@@ -32,6 +34,24 @@ prop_functorFmapWorks ints =
     func = (\i -> i * 2)
     tree1 = fmap func $ foldl addValue MyTreeEmpty ints
     tree2 = foldl addValue MyTreeEmpty $ fmap func ints
+  in
+    tree1 == tree2
+
+prop_functorWithId :: [Integer] -> Bool
+prop_functorWithId ints =
+  let
+    tree1 = foldl addValue MyTreeEmpty ints
+    tree2 = fmap id tree1
+  in
+    tree1 == tree2
+
+prop_functorCommutative :: [Integer] -> Bool
+prop_functorCommutative ints =
+  let
+    func1 = (\i -> i + 1)
+    func2 = (\i -> i * 2)
+    tree1 = fmap (func1 . func2) $ foldl addValue MyTreeEmpty ints
+    tree2 = fmap func1 $ fmap func2 $ foldl addValue MyTreeEmpty ints
   in
     tree1 == tree2
 
